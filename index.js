f5a57a7ea35b9d3b584badadef6355b0494c61aa
Origho-precious <53418429+Origho-precious@users.noreply.github.com>
2020-03-14 01:13:41 +0100

    Added All other pages

--- index.js
+++ index.js
@@ -1,31 +1,10 @@
 
 // NavBar Code
-const hamburger = document.getElementById('hamburger');
+const hamburger = document.getElementById('nav-collapser');
 const navUl = document.getElementById('nav-ul');
 
 hamburger.addEventListener('click', () => {
     navUl.classList.toggle('show')
 });
 
-// // Form Alert
-// const userName = document.getElementById('username').value;
-// const email = document.getElementById('email').value;
-// const password = document.getElementById('password').value;
-// const phoneNumber = document.getElementById('number').value;
-// const submitBtn = document.querySelector('.signup-btn');
-// const alert = document.querySelector('.signup-alert');
 
-// alert.style.display = 'none';
-
-// submitBtn.addEventListener('click', (e) => {
-//     if(userName !== null && email !== null && password !== null && phoneNumber !== null){
-//         window.location.href = "https://google.com";
-//         alert.style.display = 'block';
-//         setTimeout(() => {
-//             alert.style.display = 'none';
-//         }, 5000);
-//     }
-
-//     alert('Hello');
-//     e.preventDefault();
-// })
